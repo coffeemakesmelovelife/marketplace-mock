f7c2d14bbb999afb7eb97b61863c10b7549dd487
karim <karimodesign@gmail.com>
2018-01-26 13:53:54 +0000

    Add edit profile feature

---
+++ src/AppBundle/Services/UserManager.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Services;
+
+use Symfony\Component\DependencyInjection\ContainerInterface as Container;
+use Doctrine\ORM\EntityManager;
+use AppBundle\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserManager
+{
+    private $container;
+    private $passwordEncoder;
+
+    public function __construct(Container $container, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+        $this->container = $container;
+        return $this;
+    }
+
+
+  /**
+   * update user
+   *
+   * @param object $user
+   * @param array $params
+   *
+   * @return array
+   */
+    public function updateUser($user, $params)
+    {
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $user->setUsername($params['username']);
+        $user->setEmail($params['email']);
+
+        if($params['email'])
+        {
+            $password = $this->passwordEncoder->encodePassword($user, $params['password']);
+            $user->setPassword($password);
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        return $params;
+    }
+}
